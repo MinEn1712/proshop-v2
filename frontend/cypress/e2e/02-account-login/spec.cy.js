1094b8df4ded24c43e4db6e566e693f96b1e4082
MinEn1712 <91123918+MinEn1712@users.noreply.github.com>
2024-10-06 00:59:29 +0700

    test-02: account login

--- frontend/cypress/e2e/02-account-login/spec.cy.js
+++ frontend/cypress/e2e/02-account-login/spec.cy.js
@@ -1,5 +1,144 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+beforeEach(() => {
+  cy.visit('http://localhost:3000/login');
+});
+
+describe('verify successful login', () => {
+  it('should login successfully', () => {
+    cy.get('input#email').type('john@email.com');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+  });
+});
+
+describe('verify sign in with a non-existing account', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('abc123@email.com');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('.Toastify__toast').should('have.text', 'Invalid email or password');
+  });
+});
+
+describe('verify invalid email (does not contain @ symbol)', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('johnemail.com');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('input#email')
+      .invoke('prop', 'validationMessage')
+      .should(
+        'equal',
+        "Please include an '@' in the email address. 'johnemail.com' is missing an '@'."
+      );
+  });
+});
+
+describe('verify invalid email (does not contain a domain after @ symbol)', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('john@');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('input#email')
+      .invoke('prop', 'validationMessage')
+      .should(
+        'equal',
+        "Please enter a part following '@'. 'john@' is incomplete."
+      );
+  });
+});
+
+describe("verify invalid email (does not contain at least a dot '.')", () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('john@email');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('.Toastify__toast').should('have.text', 'Invalid email or password');
+  });
+});
+
+describe('verify invalid email (contains spaces)', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('john @email.com');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('input#email')
+      .invoke('prop', 'validationMessage')
+      .should(
+        'equal',
+        "A part followed by '@' should not contain the symbol ' '."
+      );
+  });
+});
+
+describe('verify invalid email (contains special characters)', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('john!#$@email.com');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('.Toastify__toast').should('have.text', 'Invalid email or password');
+  });
+});
+
+describe('verify invalid email (empty email)', () => {
+  it('should not login successfully', () => {
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('.Toastify__toast').should('have.text', 'Invalid email or password');
+  });
+});
+
+describe('verify incorrect password', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('john@email.com');
+    cy.get('input#password').type('1234567');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('.Toastify__toast').should('have.text', 'Invalid email or password');
+  });
+});
+
+describe('verify invalid password (empty password)', () => {
+  it('should not login successfully', () => {
+    cy.get('input#email').type('john@email.com');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('.Toastify__toast').should('have.text', 'Invalid email or password');
+  });
+});
+
+describe('verify navigate from Register page to Sign In page', () => {
+  it('should navigate to Sign In page', () => {
+    cy.get('a').contains('Register').click();
+    cy.url().should('include', '/register');
+    cy.get('a').contains('Login').click();
+    cy.url().should('include', '/login');
+  });
+});
+
+// describe('verify active session after closing the web application', () => {
+//   it('should still be logged in', () => {
+//     cy.get('button').contains('Sign Out').click();
+//     cy.get('button').contains('Sign In').click();
+//     cy.url().should('include', '/login');
+//   });
+// });
+
+// describe('verify multiple active sessions', () => {
+//   it('should still be logged in', () => {
+//     cy.get('input#email').type('john@email.com');
+//     cy.get('input#password').type('123456');
+//     cy.get('button[type="submit"]').contains('Sign In').click();
+//     cy.get('button').contains('Sign Out').click();
+//     cy.get('button').contains('Sign In').click();
+//     cy.url().should('include', '/login');
+//   });
+// });
+
+describe('verify logout', () => {
+  it('should log out successfully', () => {
+    cy.get('input#email').type('john@email.com');
+    cy.get('input#password').type('123456');
+    cy.get('button[type="submit"]').contains('Sign In').click();
+    cy.get('a#username').click();
+    cy.get('a').contains('Logout').click();
+    cy.url().should('include', '/login');
+  });
+});
